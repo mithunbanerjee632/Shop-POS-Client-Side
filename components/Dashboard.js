3c10a5209d751a6f7b150f600d1b1435c8a48ec5
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-07-15 20:59:29 +0600

    Dashboard Component Data flow

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -4,129 +4,11 @@ import DataTable from 'react-data-table-component';
 import { AreaChart, Area, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class Dashboard extends Component {
-    constructor() {
-        super();
-        this.state={
-            TotalTransaction:"100",
-            TotalIncome:"100",
-            TotalCategory:"100",
-            TotalProduct:"100"
-        }
-    }
+
     render() {
-        const data = [
-            {
-               id:1, receipt_no:'1111',
-                     receipt_date: '11/11/2022',
-                     qty :'12',
-                     total:'1200'
-            },
-            {
-                id:1, receipt_no:'1111',
-                receipt_date: '11/11/2022',
-                qty :'12',
-                total:'1200'
-            },
-            {
-                id:1, receipt_no:'1111',
-                receipt_date: '11/11/2022',
-                qty :'12',
-                total:'1200'
-            },
-            {
-                id:1, receipt_no:'1111',
-                receipt_date: '11/11/2022',
-                qty :'12',
-                total:'1200'
-            },
-            {
-                id:1, receipt_no:'1111',
-                receipt_date: '11/11/2022',
-                qty :'12',
-                total:'1200'
-            },
-            {
-                id:1, receipt_no:'1111',
-                receipt_date: '11/11/2022',
-                qty :'12',
-                total:'1200'
-            },
-            {
-                id:1, receipt_no:'1111',
-                receipt_date: '11/11/2022',
-                qty :'12',
-                total:'1200'
-            },
-        ];
 
-        const columns = [
-            {
-                name:'receipt',
-                selector:'receipt_no',
-                sortable:true
-            },
-            {
-                name:'date',
-                selector:'receipt_date',
-                sortable:true
-            },
 
-            {
-                name:'qty',
-                selector:'qty',
-                sortable:true
-            },
-            {
-                name:'total',
-                selector:'total',
-                sortable:true
-            },
-        ]
 
-        const dataChart=[
-            {
-                name: '22/11/2022',
-                uv: 4000,
-                pv: 2400,
-                amt: 2400,
-            },
-            {
-                name: '22/11/2022',
-                uv: 3000,
-                pv: 1398,
-                amt: 2210,
-            },
-            {
-                name: '22/11/2022',
-                uv: 2000,
-                pv: 9800,
-                amt: 2290,
-            },
-            {
-                name: '22/11/2022',
-                uv: 2780,
-                pv: 3908,
-                amt: 2000,
-            },
-            {
-                name: '22/11/2022',
-                uv: 1890,
-                pv: 4800,
-                amt: 2181,
-            },
-            {
-                name: '22/11/2022',
-                uv: 2390,
-                pv: 3800,
-                amt: 2500,
-            },
-            {
-                name: '22/11/2022',
-                uv: 3490,
-                pv: 4300,
-                amt: 2100,
-            },
-        ]
         return (
             <Fragment>
                 <Container>
@@ -136,7 +18,7 @@ class Dashboard extends Component {
                                 <Col className="text-center animated zoomIn p-1" sm={6} xs={6} md={6} lg={6}>
                                     <Card>
                                         <Card.Body>
-                                            <h1 className="count-number">{this.state.TotalTransaction}</h1>
+                                            <h1 className="count-number">{this.props.TotalTransaction}</h1>
                                             <h6 className="count-title">Total Transaction</h6>
                                         </Card.Body>
                                     </Card>
@@ -144,7 +26,7 @@ class Dashboard extends Component {
                                 <Col className="text-center animated zoomIn p-1" sm={6} xs={6} md={6} lg={6}>
                                     <Card>
                                         <Card.Body>
-                                            <h1 className="count-number">{this.state.TotalIncome}</h1>
+                                            <h1 className="count-number">{this.props.TotalIncome}</h1>
                                             <h6 className="count-title">Total Income</h6>
                                         </Card.Body>
                                     </Card>
@@ -153,7 +35,7 @@ class Dashboard extends Component {
                                 <Col className="text-center animated zoomIn p-1" sm={6} xs={6} md={6} lg={6}>
                                     <Card>
                                         <Card.Body>
-                                            <h1 className="count-number">{this.state.TotalCategory}</h1>
+                                            <h1 className="count-number">{this.props.TotalCategory}</h1>
                                             <h6 className="count-title">Total Category</h6>
                                         </Card.Body>
                                     </Card>
@@ -162,7 +44,7 @@ class Dashboard extends Component {
                                 <Col className="text-center animated zoomIn p-1" sm={6} xs={6} md={6} lg={6}>
                                     <Card>
                                         <Card.Body>
-                                            <h1 className="count-number">{this.state.TotalProduct}</h1>
+                                            <h1 className="count-number">{this.props.TotalProduct}</h1>
                                             <h6 className="count-title">Total Product</h6>
                                         </Card.Body>
                                     </Card>
@@ -174,10 +56,10 @@ class Dashboard extends Component {
                                         <Card.Body>
                                             <h1 className="count-title">Last 7 Days</h1>
                                             <ResponsiveContainer width="100%" height={200}>
-                                                <AreaChart  data={dataChart}>
+                                                <AreaChart  data={this.props.dataChart}>
                                                     <CartesianGrid strokeDasharray="3 3" />
                                                     <Tooltip />
-                                                    <Area type="monotone" dataKey="uv" stroke="#CB4D85" fill="#CB4D85" />
+                                                    <Area type="monotone" dataKey="income" stroke="#CB4D85" fill="#CB4D85" />
                                                 </AreaChart>
                                             </ResponsiveContainer>
                                         </Card.Body>
@@ -194,8 +76,8 @@ class Dashboard extends Component {
                                         paginationPerPage={6}
                                         pagination={true}
                                         title="Recent Transaction"
-                                        columns={columns}
-                                        data={data}/>
+                                        columns={this.props.dataTableColumns}
+                                        data={this.props.dataTable}/>
                                 </Card.Body>
                             </Card>
                         </Col>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,135 @@
+export async function getStaticProps(){
+    const dataChart=[
+        {
+            t_date: '22/11/2022',
+            income: 4000,
+
+        },
+        {
+            t_date: '22/11/2022',
+            income: 3000,
+
+        },
+        {
+            t_date: '22/11/2022',
+            income: 2000,
+
+        },
+        {
+            t_date: '22/11/2022',
+            income: 2780,
+
+        },
+        {
+            t_date: '22/11/2022',
+            income: 1890,
+
+        },
+        {
+            t_date: '22/11/2022',
+            income: 2390,
+
+        },
+        {
+            t_date: '22/11/2022',
+            income: 3490,
+
+        },
+    ];
+
+    const dataTable = [
+        {
+            id:1,
+            receipt_no:'1111',
+            receipt_date: '11/11/2022',
+            qty :'12',
+            total:'1200'
+        },
+        {
+            id:1,
+            receipt_no:'1111',
+            receipt_date: '11/11/2022',
+            qty :'12',
+            total:'1200'
+        },
+        {
+            id:1,
+            receipt_no:'1111',
+            receipt_date: '11/11/2022',
+            qty :'12',
+            total:'1200'
+        },
+        {
+            id:1,
+            receipt_no:'1111',
+            receipt_date: '11/11/2022',
+            qty :'12',
+            total:'1200'
+        },
+        {
+            id:1,
+            receipt_no:'1111',
+            receipt_date: '11/11/2022',
+            qty :'12',
+            total:'1200'
+        },
+        {
+            id:1,
+            receipt_no:'1111',
+            receipt_date: '11/11/2022',
+            qty :'12',
+            total:'1200'
+        },
+        {
+            id:1,
+            receipt_no:'1111',
+            receipt_date: '11/11/2022',
+            qty :'12',
+            total:'1200'
+        },
+    ];
+
+    const dataTableColumns = [
+        {
+            name:'receipt',
+            selector:'receipt_no',
+            sortable:true
+        },
+        {
+            name:'date',
+            selector:'receipt_date',
+            sortable:true
+        },
+
+        {
+            name:'qty',
+            selector:'qty',
+            sortable:true
+        },
+        {
+            name:'total',
+            selector:'total',
+            sortable:true
+        },
+    ];
+
+
+
+    return {
+        props: {
+            dataTableColumns:dataTableColumns,
+            dataTable:dataTable,
+            TotalTransaction:"200",
+            TotalIncome:"400",
+            TotalCategory:"600",
+            TotalProduct:"900",
+            dataChart:dataChart
+        }
+    }
+}
+
+
+
 import React, {Component} from 'react';
 import NavMenuDesktop from "../components/NavMenuDesktop";
 import Dashboard from "../components/Dashboard";
@@ -7,7 +139,15 @@ class Index extends Component {
         return (
             <div>
                <NavMenuDesktop/>
-               <Dashboard/>
+               <Dashboard
+                   dataTableColumns={this.props.dataTableColumns}
+                   dataTable={this.props.dataTable}
+                   dataChart={this.props.dataChart}
+                   TotalTransaction={this.props.TotalTransaction}
+                   TotalIncome={this.props.TotalIncome}
+                   TotalCategory={this.props.TotalCategory}
+                   TotalProduct={this.props.TotalProduct}
+               />
             </div>
         );
     }
