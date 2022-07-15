04892299a521702e0c369c183bb25d07f022b171
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-07-16 01:11:32 +0600

    category List Data flow

--- pages/category.js
+++ pages/category.js
@@ -1,11 +1,52 @@
+export async function getStaticProps(){
+    const data = [
+        {
+            id: 1,
+            cat_name: 'Laptop',
+            cat_code: 'laptop110',
+            cat_icon: 'laptop.svg',
+
+        },
+        {
+            id: 2,
+            cat_name: 'Laptop',
+            cat_code: 'laptop110',
+            cat_icon: 'laptop.svg',
+        },
+        {
+            id: 3,
+            cat_name: 'Laptop',
+            cat_code: 'laptop110',
+            cat_icon: 'laptop.svg',
+        },
+        {
+            id: 4,
+            cat_name: 'Laptop',
+            cat_code: 'laptop110',
+            cat_icon: 'laptop.svg',
+        },
+
+    ]
+
+
+    return {
+        props: {
+            dataTable:data
+        }
+    }
+}
+
+
 import React, {Component} from 'react';
 import NavMenuDesktop from "../components/NavMenuDesktop";
+import CategoryList from "../components/CategoryList";
 
 class Category extends Component {
     render() {
         return (
             <div>
                 <NavMenuDesktop/>
+                <CategoryList dataTable={this.props.dataTable}/>
             </div>
         );
     }
