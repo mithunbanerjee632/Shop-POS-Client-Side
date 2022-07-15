b8c66838e904a7c0da08c53899c2f061b965ce78
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-07-16 00:38:29 +0600

    User List Data Flow

--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,55 @@
+export async function getStaticProps(){
+    const data = [
+        {
+            id: 1,
+            fullname: 'Mithun Banerjee',
+            username: 'Mithun',
+            roll: 'admin',
+            lastactivity: '12/07/2021',
+        },
+        {
+            id: 2,
+            fullname: 'Mithun Banerjee',
+            username: 'Mithun',
+            roll: 'admin',
+            lastactivity: '12/07/2021',
+        },
+        {
+            id: 3,
+            fullname: 'Mithun Banerjee',
+            username: 'Mithun',
+            roll: 'admin',
+            lastactivity: '12/07/2021',
+        },
+        {
+            id: 4,
+            fullname: 'Mithun Banerjee',
+            username: 'Mithun',
+            roll: 'admin',
+            lastactivity: '12/07/2021',
+        },
+
+    ]
+
+
+    return {
+        props: {
+            dataTable:data
+        }
+    }
+}
+
+
 import React, {Component} from 'react';
 import NavMenuDesktop from "../components/NavMenuDesktop";
+import UserList from "../components/UserList";
 
 class Users extends Component {
     render() {
         return (
             <div>
                 <NavMenuDesktop/>
+                <UserList dataTable={this.props.dataTable}/>
             </div>
         );
     }
